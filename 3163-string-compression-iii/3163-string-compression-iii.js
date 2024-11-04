e69c54033d1b7aaa5d19849f35d3f69b8f5e5cd4
withleche <106927728+whoisrey@users.noreply.github.com>
2024-11-05 08:47:05 +0900

    Time: 65 ms (25.00%), Space: 72.2 MB (5.88%) - LeetHub

--- 3163-string-compression-iii/3163-string-compression-iii.js
+++ 3163-string-compression-iii/3163-string-compression-iii.js
@@ -4,23 +4,23 @@
  */
 var compressedString = function(word) {
     let count = 1;
-    let answer = "";
+    let answer = [];
     
     for (let i = 0; i < word.length; i++) {
         if (word[i] === word[i + 1]) {
             count++;
             if (count === 9) {
-                answer += count + word[i]
+                answer.push(count + word[i])
                 count = 0;
             }
         } else {
             if (count > 0) {
-                answer += count + word[i]    
+                answer.push(count + word[i])
             }
             
             count = 1;
         }
     }
     
-    return answer
+    return answer.join('')
 };

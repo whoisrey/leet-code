a64c06490c789f273c128491284b5dd9c8d2f9d2
withleche <106927728+whoisrey@users.noreply.github.com>
2024-11-09 09:26:51 +0900

    Time: 2 ms (29.91%), Space: 54.2 MB (20.22%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var buildArray = function(nums) {
+    const result = [];
+    
+    for (let i = 0; i < nums.length; i++) {
+        result[i] = nums[nums[i]];
+    }
+    
+    return result;
+};

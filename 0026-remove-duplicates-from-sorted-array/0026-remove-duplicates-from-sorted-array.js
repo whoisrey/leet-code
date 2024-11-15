ef246142de0ae466739c0dbb216283fa3ab821c1
withleche <106927728+whoisrey@users.noreply.github.com>
2024-11-15 11:47:56 +0900

    Time: 2 ms (36.50%), Space: 51.9 MB (85.83%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,19 +3,18 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    let isRemoved = false;
+    // let isRemoved = false;
     
-    for (let i = 0; i < nums.length - 1; i++) {
-        if (nums[i] === nums[i + 1]) {
-            nums.splice(i , 1)
-            isRemoved = true;
-            break;
+    for (let i = nums.length - 1; 0 < i; i--) {
+        if (nums[i - 1] === nums[i]) {
+            nums.splice(i, 1)
+            // isRemoved = true;
         }
     }
     
-    if (isRemoved) {
-        removeDuplicates(nums);
-    }
+    // if (isRemoved) {
+    //     removeDuplicates(nums);
+    // }
     
     return nums.length;
 };

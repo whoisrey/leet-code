7dc511bca6c728a05b7e1a08487f14f75a03bd5d
withleche <106927728+whoisrey@users.noreply.github.com>
2024-11-20 11:49:38 +0900

    Time: 49 ms (36.71%), Space: 52.1 MB (63.80%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -4,14 +4,17 @@
  */
 var longestPalindrome = function(s) {
   let max = '';
+    
   for (let i = 0; i < s.length; i++) {
     for (let j = 0; j < 2; j++) {
       let left = i;
       let right = i + j;
+        
       while (s[left] && s[left] === s[right]) {
         left--;
         right++;
       }
+        
       if ((right - left - 1) > max.length) {
         max = s.substring(left + 1, right);
       }

4e0d61e4f9b29857d4c8cea412cdd6bbf9c16fbc
withleche <106927728+whoisrey@users.noreply.github.com>
2024-11-21 09:41:10 +0900

    Time: 0 ms (100.00%), Space: 48.8 MB (73.22%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,24 +3,27 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    if (!s.includes(" ")) return s.length;
+//     if (!s.includes(" ")) return s.length;
     
-    let lastIndex = 0;
-    let firstIndex = 0;
+//     let lastIndex = 0;
+//     let firstIndex = 0;
     
-    for (let i = s.length - 1; 0 <= i; i--) {
-        if (s[i] !== " ") {
-            lastIndex = i;
-            break;
-        }
-    }
+//     for (let i = s.length - 1; 0 <= i; i--) {
+//         if (s[i] !== " ") {
+//             lastIndex = i;
+//             break;
+//         }
+//     }
     
-    for (let i = lastIndex; 0 <= i; i--) {
-        if (s[i] === " ") {
-            firstIndex = i + 1;
-            break;
-        }
-    }
+//     for (let i = lastIndex; 0 <= i; i--) {
+//         if (s[i] === " ") {
+//             firstIndex = i + 1;
+//             break;
+//         }
+//     }
     
-    return lastIndex + 1 - firstIndex;
+//     return lastIndex + 1 - firstIndex;
+    const b = s.trim().split(" ")
+    
+    return b[b.length - 1].length
 };

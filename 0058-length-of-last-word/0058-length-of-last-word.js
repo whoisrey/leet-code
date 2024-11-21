b81a164fc596ff9c5ecca0afae7ee02bf3a91155
withleche <106927728+whoisrey@users.noreply.github.com>
2024-11-21 09:01:03 +0900

    Time: 0 ms (100.00%), Space: 49.3 MB (11.11%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    if (!s.includes(" ")) return s.length;
+    
+    let lastIndex = 0;
+    let firstIndex = 0;
+    
+    for (let i = s.length - 1; 0 <= i; i--) {
+        if (s[i] !== " ") {
+            lastIndex = i;
+            break;
+        }
+    }
+    
+    for (let i = lastIndex; 0 <= i; i--) {
+        if (s[i] === " ") {
+            firstIndex = i + 1;
+            break;
+        }
+    }
+    
+    return s.substring(firstIndex, lastIndex + 1).length;
+};

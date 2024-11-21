2f236586e99c8febbc96206c042e4b35d7216a87
withleche <106927728+whoisrey@users.noreply.github.com>
2024-11-22 08:30:32 +0900

    Time: 0 ms (100.00%), Space: 52.6 MB (78.90%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    const firstElement = nums[0];
+    const lastElement = nums[nums.length - 1];
+    const isNearFirst = firstElement - target < lastElement - target;
+    let answer = -1;
+    
+    if (isNearFirst) {
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[i] === target) {
+                answer = i;
+                break;
+            } else if (nums[i] > target) {
+                break;
+            }
+        }
+    } else {
+        for (let i = nums.length - 1; 0 <= i; i--) {
+            if (nums[i] === target) {
+                answer = nums.length - 1- i;
+                break;
+            } else if (nums[i] < target) {
+                break;
+            }
+        }
+    }
+    
+    return answer;
+};

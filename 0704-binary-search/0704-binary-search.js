ff96002a7e02b03147981983380ab936cef7fcc2
withleche <106927728+whoisrey@users.noreply.github.com>
2024-11-22 09:00:06 +0900

    Time: 0 ms (100.00%), Space: 51.9 MB (97.64%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -4,33 +4,19 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    const firstElement = nums[0];
-    const lastElement = nums[nums.length - 1];
-    const isNearFirst = firstElement - target < lastElement - target;
-    const includeTarget = lastElement - target >= 0;
-    let answer = -1;
+    let firstIndex = 0;
+    let lastIndex = nums.length - 1;
     
-    if (!includeTarget) return -1;
-    
-    if (isNearFirst) {
-        for (let i = 0; i < nums.length; i++) {
-            if (nums[i] === target) {
-                answer = i;
-                break;
-            } else if (nums[i] > target) {
-                break;
-            }
-        }
-    } else {
-        for (let i = nums.length - 1; 0 <= i; i--) {
-            if (nums[i] === target) {
-                answer = nums.length - 1- i;
-                break;
-            } else if (nums[i] < target) {
-                break;
-            }
-        }
+    while (firstIndex <= lastIndex) {
+	    let midIndex = Math.floor((firstIndex + lastIndex) / 2)
+	    if (nums[midIndex] === target) {
+		    return midIndex;
+		  } else if (nums[midIndex] < target) {
+			  firstIndex++;
+			} else {
+				lastIndex--;
+			}
     }
-    
-    return answer;
+        
+    return -1;
 };

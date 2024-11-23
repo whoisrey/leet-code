0d06ca467aa7d9ac91c0c65c630b627b20e3aff6
withleche <106927728+whoisrey@users.noreply.github.com>
2024-11-23 17:42:03 +0900

    Time: 0 ms (100.00%), Space: 49.1 MB (36.09%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -3,22 +3,15 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    const lastIndex = digits.length - 1;
-    
-    for (let i = lastIndex; 0 <= i; i--) {
-        if (digits[i] === 9) {
-            if (i === 0) {
-                digits[i] = 0;
-                digits.unshift(1);
-                break;
-            }
-            
-            digits[i] = 0;            
-        } else {
-            digits[i]++;
-            break;
-        }
+  for(let i = digits.length - 1; i >= 0; i --){
+    if(digits[i] === 9){
+      digits[i] = 0;
+    } else {
+      digits[i]++;
+      
+      return digits;
     }
-    
-    return digits;
+  }
+  
+  return [1, ...digits];
 };

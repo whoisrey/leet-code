1115e00cd80012809467827db3e52eb0273ab3d9
withleche <106927728+whoisrey@users.noreply.github.com>
2024-11-25 08:32:29 +0900

    Time: 0 ms (100.00%), Space: 49 MB (18.32%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -6,9 +6,7 @@ var climbStairs = function(n) {
     const answer = [1, 1];
     
     for (let i = 2; i <= n; i++) {
-        if (2 <= i) {
-            answer.push(answer[i - 2] + answer[i - 1]);
-        }
+        answer.push(answer[i - 2] + answer[i - 1]);
     }
     
     return answer[n]

c61513ba2cdd0182782644818ddb301dce5f7d70
withleche <106927728+whoisrey@users.noreply.github.com>
2024-11-27 14:49:29 +0900

    Time: 0 ms (100.00%), Space: 48.4 MB (94.78%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -17,35 +17,4 @@ var merge = function(nums1, m, nums2, n) {
     }
     
     nums1.sort((a, b) => a - b);
-    
-    console.log(nums1)
-     
-//     if (n === 0) return m;
-//     if (m === 0) {
-//         for (let i = 0; i < n; i++) {
-//             nums1[i] = nums2[i];
-//         }
-//     } else {
-//         const temp = [];
-        
-//         for (const i of nums1) {
-//             if (i !== 0) temp.push(i);
-//         }
-        
-//         for (const i of nums2) {
-//             if (i !== 0) temp.push(i);
-//         }
-        
-//         const rest = m + n - temp.length;
-        
-//         for (let i = 0; i < rest; i++) {
-//             temp.push(0);
-//         }
-        
-//         temp.sort((a, b) => a - b);
-        
-//         for (let i = 0; i < nums1.length; i++) {
-//             nums1[i] = temp[i];
-//         }
-//     }
 };

09a74b146d5df9da5bb5350c83bd00098495b23c
withleche <106927728+whoisrey@users.noreply.github.com>
2024-11-29 08:02:33 +0900

    Time: 14 ms (32.08%), Space: 55.4 MB (10.70%) - LeetHub

---
+++ 0136-single-number/0136-single-number.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    if (nums.length === 1) return nums[0];
+    
+    const temp = {};
+    
+    for (const i of nums) {
+        if (!temp[i]) {
+            temp[i] = 1;
+        } else {
+            temp[i]++;
+        }
+    }
+    
+    
+    for (const i in temp) {
+        if (temp[i] === 1) return parseInt(i);
+    }
+
+};

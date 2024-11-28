676258fa3f010c5454784b4ca07c3ccf1382c0b3
withleche <106927728+whoisrey@users.noreply.github.com>
2024-11-28 10:41:34 +0900

    Time: 3 ms (87.03%), Space: 50.3 MB (82.87%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.js
+++ 0409-longest-palindrome/0409-longest-palindrome.js
@@ -6,15 +6,10 @@ var longestPalindrome = function(s) {
   const temp = {};
   let answer = 0;
 
-  for (let char of s) {
-    if (!temp[char]) {
-        temp[char] = 1;
-    } else {
-        temp[char]++;
-    }
-    // temp[char] = (temp[char] || 0) + 1;
+  for (let char of s) {      
+    temp[char] ? temp[char]++ : temp[char] = 1;
       
-    if (temp[char] % 2 == 0) {
+    if (temp[char] % 2 === 0) {
         answer += 2;
     }
   }

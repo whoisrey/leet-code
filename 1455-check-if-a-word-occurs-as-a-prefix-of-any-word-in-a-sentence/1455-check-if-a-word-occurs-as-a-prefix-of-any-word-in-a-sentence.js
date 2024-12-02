d82e539ec3229301502afc1b66d1da4aec5bbfe6
withleche <106927728+whoisrey@users.noreply.github.com>
2024-12-02 09:14:31 +0900

    Time: 0 ms (100.00%), Space: 49.1 MB (9.80%) - LeetHub

---
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} sentence
+ * @param {string} searchWord
+ * @return {number}
+ */
+var isPrefixOfWord = function(sentence, searchWord) {
+    const arr = sentence.split(' ');
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].includes(searchWord)) {
+            if (arr[i].slice(0, searchWord.length) === searchWord) return i + 1;
+        }
+    }
+    
+    return -1;
+};

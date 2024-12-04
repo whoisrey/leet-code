7915214097a4814b5f0e1dfb3308846ad3172005
withleche <106927728+whoisrey@users.noreply.github.com>
2024-12-05 08:49:37 +0900

    Time: 0 ms (100.00%), Space: 51.9 MB (6.85%) - LeetHub

---
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @param {number} m
+ * @return {number}
+ */
+var differenceOfSums = function(n, m) {
+    let num1 = 0;
+    let num2 = 0;
+    let pointer = 1;
+    
+    while (pointer < n + 1) {
+        if (pointer % m === 0) {
+            num1 += pointer;
+        } else {
+            num2 += pointer;
+        }
+        
+        pointer++;
+    }
+    
+    return num2 - num1;
+};

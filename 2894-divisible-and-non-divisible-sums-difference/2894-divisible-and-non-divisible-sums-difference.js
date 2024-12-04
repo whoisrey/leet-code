340cc652b923422d8f3ca1be85cdb2e8b432da6b
withleche <106927728+whoisrey@users.noreply.github.com>
2024-12-05 08:53:20 +0900

    Time: 0 ms (100.00%), Space: 50.4 MB (82.44%) - LeetHub

--- 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.js
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.js
@@ -4,19 +4,21 @@
  * @return {number}
  */
 var differenceOfSums = function(n, m) {
-    let num1 = 0;
-    let num2 = 0;
-    let pointer = 1;
+//     let num1 = 0;
+//     let num2 = 0;
+//     let pointer = 1;
     
-    while (pointer < n + 1) {
-        if (pointer % m !== 0) {
-            num1 += pointer;
-        } else {
-            num2 += pointer;
-        }
+//     while (pointer < n + 1) {
+//         if (pointer % m !== 0) {
+//             num1 += pointer;
+//         } else {
+//             num2 += pointer;
+//         }
         
-        pointer++;
-    }
+//         pointer++;
+//     }
     
-    return num1 - num2;
+//     return num1 - num2;
+        const div= parseInt(n/m)
+    return (((n*(n+1))/2) - (2*m*((div*(div+1))/2)))
 };

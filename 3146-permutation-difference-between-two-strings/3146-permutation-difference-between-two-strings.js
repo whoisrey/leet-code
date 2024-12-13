442b6b6778e1e6478c111c96a9aba181025876cd
withleche <106927728+whoisrey@users.noreply.github.com>
2024-12-14 08:47:51 +0900

    Time: 10 ms (14.52%), Space: 55.7 MB (5.86%) - LeetHub

--- 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.js
+++ 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.js
@@ -4,10 +4,18 @@
  * @return {number}
  */
 var findPermutationDifference = function(s, t) {
+    const firstHash = {};
+    const secondHash = {};
     let answer = 0;
     
     for (let i = 0; i < s.length; i++) {
-        answer += Math.abs(i - t.indexOf(s[i]));
+        firstHash[s[i]] = i;
+        secondHash[t[i]] = i;
+    }
+    
+    
+    for (let i = 0; i < s.length; i++) {        
+        answer += Math.abs(firstHash[s[i]] - secondHash[s[i]]);
     }
     
     return answer;

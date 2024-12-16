b88c3d923ef333772751f028b7f7d198c5f8f609
withleche <106927728+whoisrey@users.noreply.github.com>
2024-12-16 09:17:37 +0900

    Time: 70 ms (6.78%), Space: 82.5 MB (68.03%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.js
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.js
@@ -4,19 +4,26 @@
  * @return {number[]}
  */
 var pivotArray = function(nums, pivot) {
-    const answer = [];
-    
-    for (const num of nums) {
-        if (num < pivot) answer.push(num);
-    }
-    
-    for (const num of nums) {
-        if (num === pivot) answer.push(num);
-    }
-    
-    for (const num of nums) {
-        if (num > pivot) answer.push(num);
-    }
-    
-    return answer;
+		const length = nums.length;
+		const output = [];
+
+		let start = 0;
+		let end = length - 1;
+
+		let pointer1 = 0;
+		let pointer2 = length - 1;
+	
+		while (start < nums.length) {
+			if (nums[start] < pivot) output[pointer1++] = nums[start];
+			if (nums[end] > pivot) output[pointer2--] = nums[end];
+	
+			start++;
+			end--;
+		}
+	
+		while (pointer1 <= pointer2) {
+			output[pointer1++] = pivot;
+		}
+	
+		return output;
 };

3012ab1b3a5e0930760ff649d08680ad83c2295f
withleche <106927728+whoisrey@users.noreply.github.com>
2024-12-18 09:11:13 +0900

    Time: 13 ms (21.54%), Space: 57.3 MB (13.85%) - LeetHub

--- 3271-hash-divided-string/3271-hash-divided-string.js
+++ 3271-hash-divided-string/3271-hash-divided-string.js
@@ -7,10 +7,12 @@ var stringHash = function(s, k) {
     const array = [];
     const point = s.length / k;
     let count = 0;
+    let answer = "";
     
     while (count < point) {
         let string = "";
         let sum = 0;
+        let str = "";
         for (let i = k * count; i < k * (count + 1); i++) {
             string += s[i];
         }
@@ -19,32 +21,18 @@ var stringHash = function(s, k) {
             sum += string.charCodeAt(i) - 97
         }
         
-        array.push(sum);
+        // array.push(sum);
+        
+        str = String.fromCodePoint(sum % 26 + 97);
+        answer += str
         
         count++;
     }
     
-    const arr = array.map((element) => String.fromCodePoint(element % 26 + 97));
+    return answer;
     
-    return arr.reduce((a, b) => a + b)
-    // console.log(arr)
-    // console.log(String.fromCodePoint(97))
+    // const arr = array.map((element) => String.fromCodePoint(element % 26 + 97));
     
-//     for (let i = 0; i < s.length; i++) {
-//         if (i < k) {
-//             firstString += s[i];
-//         } else {
-//             secondString += s[i];
-//         }
-//     }
-    
-//     for (let i = 0; i < firstString.length; i++) {
-//         firstSum += firstString.charCodeAt(i) - 97;
-//     }
-    
-//     for (let i = 0; i < secondString.length; i++) {
-//         secondSum += secondString.charCodeAt(i) - 97;
-//     }
-    
+    // return arr.reduce((a, b) => a + b)
     
 };

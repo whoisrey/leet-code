3dcee60afc976c2b434a0e173bd7859d2570201b
withleche <106927728+whoisrey@users.noreply.github.com>
2024-12-24 11:41:46 +0900

    Time: 92 ms (34.41%), Space: 61.1 MB (39.51%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.js
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.js
@@ -4,28 +4,33 @@
  * @return {number}
  */
 var countConsistentStrings = function(allowed, words) {
-    function isAllowed(firstWord, secondWord) {
-        let count = 0;
+//     function isAllowed(firstWord, secondWord) {
+//         let count = 0;
         
-        for (const i of secondWord) {
-            if(firstWord.includes(i)) count++;
-        }
+//         for (const i of secondWord) {
+//             if(firstWord.includes(i)) count++;
+//         }
         
-        return count === secondWord.length ? true : false;
-    }
+//         return count === secondWord.length ? true : false;
+//     }
     
-    let count = 0;
+//     let count = 0;
     
-    for (const word of words) {
-        let string = "";
+//     for (const word of words) {
+//         let string = "";
         
-        for (const i of word) {
-            if(!string.includes(i)) string += i;
-        }
-        console.log(isAllowed(allowed, string))
-        console.log(string)
-        if (isAllowed(allowed, string)) count++;
-    }
+//         for (const i of word) {
+//             if(!string.includes(i)) string += i;
+//         }
+        
+//         if (isAllowed(allowed, string)) count++;
+//     }
     
-    return count;
+//     return count;
+    
+    let set = new Set(allowed)
+    
+    return words.reduce((a, w) => {
+        return w.split('').every(l => set.has(l)) ? ++a : a
+    }, 0)
 };

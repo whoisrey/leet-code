a567d7a28140ab0d0174b226b786de0c81b6cae3
withleche <106927728+whoisrey@users.noreply.github.com>
2025-01-03 10:44:14 +0900

    Time: 1 ms, Memory: 49.5 MB - LeetHub

--- 2051-81-1422-maximum-score-after-splitting-a-string/2051-81-1422-maximum-score-after-splitting-a-string.js
+++ 2051-81-1422-maximum-score-after-splitting-a-string/2051-81-1422-maximum-score-after-splitting-a-string.js
@@ -4,20 +4,19 @@
  */
 var maxScore = function(s) {
     let answer = 0;
-    let zero = 0;
-    let one = 0;
-
+    let left = 0;
+    let right = 0;
+    
     for (const i of s) {
-        if (i === "1") one++;
+        if (i === "1") right++;
     }
-
+    
     for (let i = 0; i < s.length - 1; i++) {
-        if (s[i] === "0") zero++;
-        if (s[i] === "1") one--;
-
-        answer = Math.max(answer, zero + one);
+        if (s[i] === "0") left++;
+        if (s[i] === "1") right--;
+        
+        answer = Math.max(answer, left + right);
     }
-
+    
     return answer;
-
 };

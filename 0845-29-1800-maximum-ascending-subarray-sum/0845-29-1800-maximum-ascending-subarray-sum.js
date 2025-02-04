0c48156f3c59f19fe8896a1e5f339338144fc489
withleche <106927728+whoisrey@users.noreply.github.com>
2025-02-04 14:10:05 +0900

    Time: 0 ms, Memory: 49.1 MB - LeetHub

---
+++ 0845-29-1800-maximum-ascending-subarray-sum/0845-29-1800-maximum-ascending-subarray-sum.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxAscendingSum = function(nums) {
+    const cache = [];
+    let sum = 0;
+    let pointer = 0;
+    
+    while (pointer < nums.length) {
+        if (nums[pointer] < nums[pointer + 1]) {
+            sum += nums[pointer];
+        } else if (nums[pointer] > nums[pointer + 1] || !nums[pointer + 1] || nums[pointer] === nums[pointer + 1]) {
+            sum += nums[pointer];
+            cache.push(sum);
+            sum = 0;
+        } else if (nums[pointer] === nums[pointer + 1]) {
+            sum = 0;
+        }
+        
+        pointer++;
+    }
+    
+    return Math.max(...cache);
+};

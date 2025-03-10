751bea78a518da7bdb687dc7f888845193488089
withleche <106927728+whoisrey@users.noreply.github.com>
2025-03-10 20:11:41 +0900

    Time: 875 ms (10%), Space: 69.6 MB (20%) - LeetHub

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.js
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.js
@@ -7,24 +7,37 @@ var countOfSubstrings = function(word, k) {
     const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
 
     function atleast(k, word) {
-        let count = 0, consonantCount = 0, left = 0, freq = new Map();
-        let n = word.length;
+        let count = 0
+        let consonantCount = 0
+        let left = 0
+        let freq = new Map();
+        let wordLength = word.length;
 
-        for (let right = 0; right < n; right++) {
+        for (let right = 0; right < wordLength; right++) {
             let ch = word[right];
-            if (!vowels.has(ch)) consonantCount++;
+            if (!vowels.has(ch)) {
+                consonantCount++;
+            }
 
             freq.set(ch, (freq.get(ch) || 0) + 1);
 
             while (consonantCount >= k && hasAllVowels(freq)) {
-                count += (n - right);
+                count += (wordLength - right);
                 let leftChar = word[left];
-                if (!vowels.has(leftChar)) consonantCount--;
+                if (!vowels.has(leftChar)) {
+                    consonantCount--;
+                }
+
                 freq.set(leftChar, freq.get(leftChar) - 1);
-                if (freq.get(leftChar) === 0) freq.delete(leftChar);
+
+                if (freq.get(leftChar) === 0) {
+                    freq.delete(leftChar);
+                }
+                
                 left++;
             }
         }
+
         return count;
     }
 
